76e8a7ca10afcbca0a77e9ef729245149eccd6d2
Patrick Nicholson <>
2021-11-17 17:26:35 -0500

    Initializes Bot Repository

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+namespace DiscordBot_DSharp{
+    class Program{
+        static void Main(string[] args){
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(){
+
+            var discord = new DiscordClient(new DiscordConfiguration(){
+                Token = "Token",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All
+            });
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+
+    }
+}
